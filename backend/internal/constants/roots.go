1316fda8456052c5227357a93b3047ecffb5e8f4
jhanvi857 <jhanvip8507@gmail.com>
2026-05-22 12:31:39 +0530

    add : root servers

--- backend/internal/constants/roots.go
+++ backend/internal/constants/roots.go
@@ -1,1 +1,9 @@
 package constants
+
+// Root DNS servers (global entry points of DNS system)
+var RootServers = []string{
+	"198.41.0.4",   // a.root-servers.net
+	"199.9.14.201", // b.root-servers.net
+	"192.33.4.12",  // c.root-servers.net
+	"199.7.91.13",  // d.root-servers.net
+}
