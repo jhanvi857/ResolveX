e164a8c360119033076655eaa7e0e2cec9282f09
jhanvi857 <jhanvip8507@gmail.com>
2026-05-22 11:27:26 +0530

    feat : add response models

--- backend/internal/models/dns_response.go
+++ backend/internal/models/dns_response.go
@@ -1,1 +1,13 @@
 package models
+
+type Hop struct {
+	Server string `json:"Server"`
+	Time   int    `json:"time"`
+}
+type DnsResponse struct {
+	Domain   string `json:"domain"`
+	Ip       string `json:"ip"`
+	CacheHit bool   `json:"cacheHit"`
+	Hops     []Hop  `json:"hops"`
+	Latency  int    `json:"Latency"`
+}
