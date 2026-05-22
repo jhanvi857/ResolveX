3b3fcb8d00a887127e4fa83ff81d4b407a6b4668
jhanvi857 <jhanvip8507@gmail.com>
2026-05-22 12:42:22 +0530

    add : quer.go with google DNS server as of now for base

--- backend/internal/resolver/query.go
+++ backend/internal/resolver/query.go
@@ -1,1 +1,17 @@
 package resolver
+
+import (
+	"github.com/miekg/dns"
+)
+
+func Query(domain string) (*dns.Msg, error) {
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(domain), dns.TypeA)
+	c := new(dns.Client)
+	resp, _, err := c.Exchange(
+		m,
+		"8.8.8.8:53",
+	)
+
+	return resp, err
+}
