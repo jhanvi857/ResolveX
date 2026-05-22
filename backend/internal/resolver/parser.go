803b8990a2a763a47b34e85fa5627ce98dbf5b3c
jhanvi857 <jhanvip8507@gmail.com>
2026-05-22 12:32:12 +0530

    add : parser to extract IP

--- backend/internal/resolver/parser.go
+++ backend/internal/resolver/parser.go
@@ -1,1 +1,12 @@
 package resolver
+
+import "github.com/miekg/dns"
+
+func ExtractIp(Msg *dns.Msg) string {
+	for _, ans := range Msg.Answer {
+		if record, ok := ans.(*dns.A); ok {
+			return record.A.String()
+		}
+	}
+	return ""
+}
